992be4a148ea3c391c3457eb53cbaf7b7266534f
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-05-17 16:27:25 +0530

    Nav + home page hero section

---
+++ src/components/Nav.js
@@ -0,0 +1,145 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    // <div className="container">
+    //   <nav class="navbar navbar-light">
+    //     <div
+    //       class="card shadow "
+    //       style={{ marginTop: "-10px", border: "none" }}
+    //     >
+    //       <div class="card-body" style={{ padding: "1rem" }}>
+    //         <a class="navbar-brand" href="#">
+    //           <img
+    //             src={require("../img/logo.png")}
+    //             width="150"
+    //             height="42"
+    //             alt=""
+    //           />
+    //         </a>
+    //       </div>
+    //     </div>
+    //     <div className="navMenu">
+    //       <div
+    //         className="row"
+    //         style={{ fontSize: "13.5px", alignContent: "right" }}
+    //       >
+    //         <p>Login/Sign Up</p>
+    //         <p>Gurgaon</p>
+    //         <p>999-888-000-5</p>
+    //       </div>
+    //       <div
+    //         className="row"
+    //         style={{ marginTop: "-5px", fontSize: "17.5px" }}
+    //       >
+    //         <p>Health Deals</p> |<p>Labs</p> |<p>Blog</p> |<p>Contact Us</p>
+    //       </div>
+    //     </div>
+    //   </nav>
+    // </div>
+    <nav class="navbar navbar-expand-lg navbar-light" style={{ zIndex: "9" }}>
+      <div className="container">
+        <div
+          class="card shadow "
+          style={{ marginTop: "-10px", border: "none" }}
+        >
+          <div class="card-body" style={{ padding: "1rem" }}>
+            <a class="navbar-brand" href="#">
+              <img
+                src={require("../img/logo.png")}
+                width="150"
+                height="42"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            {/* <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li> */}
+          </ul>
+          <div class="div-inline my-2 my-lg-0">
+            <div className="navMenu">
+              <div
+                className="row"
+                style={{ fontSize: "13.5px", justifyContent: "flex-end" }}
+              >
+                {/* <p>Login/Sign Up</p>
+                <p>Gurgaon</p>
+                <p>999-888-000-5</p> */}
+                <ul class="navbar-nav" style={{ justifyContent: "right" }}>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Login/Sign Up
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Gurgaon
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      999-888-000-5
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div
+                className="row"
+                style={{ marginTop: "-5px", fontSize: "17.5px" }}
+              >
+                {/* <p>Health Deals</p> |<p>Labs</p> |<p>Blog</p> |<p>Contact Us</p> */}
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Health Deals<span className="menu-separator">|</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Labs<span className="menu-separator">|</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Blogs<span className="menu-separator">|</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
