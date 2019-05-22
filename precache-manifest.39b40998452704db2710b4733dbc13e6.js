c3c7669176090d54dfaaffbe605bb5759e2a9364
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-05-22 09:24:10 +0530

    Updates

---
+++ precache-manifest.39b40998452704db2710b4733dbc13e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bc7050f7634aac2054a6dc6f059b74",
+    "url": "/saral/index.html"
+  },
+  {
+    "revision": "2a1d767d4acab2099411",
+    "url": "/saral/static/css/main.08541765.chunk.css"
+  },
+  {
+    "revision": "9c225ca6615b13377d8a",
+    "url": "/saral/static/js/2.18e56f69.chunk.js"
+  },
+  {
+    "revision": "2a1d767d4acab2099411",
+    "url": "/saral/static/js/main.4711dc0b.chunk.js"
+  },
+  {
+    "revision": "1831843e9bc787c92ac1",
+    "url": "/saral/static/js/runtime~main.26e20269.js"
+  },
+  {
+    "revision": "5ebffabb654e0bcef1c82e9744bd728d",
+    "url": "/saral/static/media/2nd_page_bg.5ebffabb.jpg"
+  },
+  {
+    "revision": "0c5d77f9dfbc15c69a14e34c1c45ba76",
+    "url": "/saral/static/media/3rd_page_bg.0c5d77f9.jpg"
+  },
+  {
+    "revision": "f4be56dc9aedcd201299eacdc177bea4",
+    "url": "/saral/static/media/NABL_Delhi.f4be56dc.jpg"
+  },
+  {
+    "revision": "27b19b2bf350a1b27df42f28a52ceddf",
+    "url": "/saral/static/media/homepage_bg.27b19b2b.jpg"
+  },
+  {
+    "revision": "f561acb10586ead85efddd82565146ff",
+    "url": "/saral/static/media/logo.f561acb1.png"
+  }
+]);
